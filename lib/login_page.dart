678566868690bbc71c86dc0066bcad6e6038f639
Hrvoje Čukman <hrvoje.cukman@q.agency>
2021-12-23 18:06:07 +0100

    Added login with google

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:login_template_firebase/repositories/auth_repository.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: MaterialButton(
+        color: Colors.blue,
+        onPressed: () async {
+          await AuthRepository.signInWithGoogle();
+        },
+        child: const Text("Login with google"),
+      ),
+    );
   }
 }
