74269367e2a86c884324f08c43b7492f7627f48d
Hrvoje Čukman <hrvoje.cukman@q.agency>
2021-12-24 18:42:25 +0100

    Added flavors to app

--- lib/app.dart
+++ lib/app.dart
@@ -1,21 +1,62 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:login_template_firebase/base_scaffold.dart';
 import 'package:login_template_firebase/cubits/auth_cubit.dart';
 import 'package:login_template_firebase/login_page.dart';
+import 'package:login_template_firebase/pages/base_scaffold.dart';
 
 import 'firebase_options.dart';
+import 'flavors.dart';
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: F.title,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: _flavorBanner(
+        child: const MyApp(),
+        show: kDebugMode,
+      ),
+    );
+  }
+
+  Widget _flavorBanner({
+    required Widget child,
+    bool show = true,
+  }) =>
+      show
+          ? Banner(
+              child: child,
+              location: BannerLocation.topStart,
+              message: F.name,
+              color: Colors.green.withOpacity(0.6),
+              textStyle: const TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 12.0,
+                letterSpacing: 1.0,
+              ),
+              textDirection: TextDirection.ltr,
+            )
+          : Container(
+              child: child,
+            );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   _AppState createState() => _AppState();
 }
 
-class _AppState extends State<App> {
+class _AppState extends State<MyApp> {
   /// The future is part of the state of our widget. We should not call `initializeApp`
   /// directly inside [build].
   final Future<FirebaseApp> _initialization = Firebase.initializeApp(
